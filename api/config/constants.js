85bc6e1d58535ff25a77bc55f9c732bed4ed1798
azrael <azrael@imatlas.com>
2015-04-06 14:52:59 +0800

    赞和评论的通知

--- api/config/constants.js
+++ api/config/constants.js
@@ -13,3 +13,15 @@ exports.LABEL_TYPE_SUBJECT_ARTICLE = 2;
 
 // 文章关联的标签
 exports.LABEL_TYPE_ARTICLE = 3;
+
+// 消息通知的类型
+// 赞
+exports.NOTIFY_STAR = 1;
+// @
+exports.NOTIFY_AT = 2;
+// 回复
+exports.NOTIFY_REPLY = 4;
+// 聊天消息
+exports.NOTIFY_MSG = 8;
+// 主题邀请
+exports.NOTIFY_SUBJ_INVITE = 16;
--- api/modules/notification.js
+++ api/modules/notification.js
@@ -1,5 +1,28 @@
+var EventProxy = require('eventproxy');
+var co = require('co');
+var ERR = require('../errorcode');
+var Logger = require('../logger');
+var config = require('../config');
+var db = require('../modules/db');
 
-// TODO
-exports.notify = function(who, atWhatTime, doWhatThing, toWho, then, message){
 
+
+exports.notify = function(connection, who, doWhat, toWho, withData, message){
+
+    return connection.yieldQuery('INSERT INTO notification SET ?', {
+        who: who,
+        doWhat: doWhat,
+        toWho: toWho,
+        withData: withData ? JSON.stringify(withData) : '',
+        message: message || ''
+    });
+
+};
+
+exports.read = function(connection, notifyId){
+
+    return connection.yieldQuery('UPDATE notification SET ? WHERE id = ?', [{
+        hasRead: 1,
+        readTime: new Date()
+    }, notifyId]);
 };
