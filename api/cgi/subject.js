9f00d888f47ce257b0ed3ae421941c7c11c0cbbb
horde <myhorde@vip.qq.com>
2015-04-23 23:05:23 +0800

    bug fix

--- api/cgi/subject.js
+++ api/cgi/subject.js
@@ -416,7 +416,7 @@ exports.list = function*(req, res) {
         yield req.conn.yieldQuery(sql);
     var total = rows[0].count;
 
-    sql = 'SELECT s.*, u.name AS creatorName, ' + '(SELECT COUNT(DISTINCT su.user_id) FROM subject_user su WHERE su.subject_id = s.id) AS memberCount, ' + '(SELECT COUNT(DISTINCT sr.id) FROM subject_resource sr WHERE sr.subject_id = s.id) AS resourceCount,(SELECT COUNT(art.id) FROM article art WHERE art.subject_id = s.id) AS articleCount  ' + 'FROM subject s, user u WHERE ';
+    sql = 'SELECT s.*, u.name AS creatorName, ' + '(SELECT COUNT(DISTINCT su.user_id) FROM subject_user su WHERE su.subject_id = s.id) AS memberCount, ' + '(SELECT COUNT(DISTINCT sr.id) FROM subject_resource sr WHERE sr.subject_id = s.id) AS resourceCount,(SELECT COUNT(art.id) FROM article art WHERE art.subject_id = s.id) AS articleCount  ' + 'FROM subject s, user u WHERE s.creator = u.id and ';
 
     //dbParams['s.creator'] = 'u.id';
     sql += req.dbPrepare(dbParams);
